488b5af6a4abaaebda45eb2ed9df1aca8624744a
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-22 19:44:01 +0300

    Use class-based theme instead of system preference

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
