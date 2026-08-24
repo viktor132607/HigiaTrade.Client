f0b61d5b3ebd61027d96d962e77e150a932c0656
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-24 20:10:00 +0300

    Show brand field in product editor

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AdminProductsLegacy from "../../components/admin/AdminProductsLegacy";
+import ProductBrandEnhancer from "../../components/admin/ProductBrandEnhancer";
 import ProductDescriptionEnhancer from "../../components/admin/ProductDescriptionEnhancer";
 import ProductStockEnhancer from "../../components/admin/ProductStockEnhancer";
 
 const AdminProducts = () => (
   <>
+    <ProductBrandEnhancer />
     <ProductDescriptionEnhancer />
     <ProductStockEnhancer />
     <AdminProductsLegacy />
