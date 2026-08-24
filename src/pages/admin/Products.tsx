4fd9f75e42f5be74f9983d55fd1349b8d2d4a1b3
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-24 21:06:06 +0300

    Add new product status controls to admin products

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import AdminProductsLegacy from "../../components/admin/AdminProductsLegacy";
 import ProductBrandEnhancer from "../../components/admin/ProductBrandEnhancer";
 import ProductDescriptionEnhancer from "../../components/admin/ProductDescriptionEnhancer";
+import ProductNewStatusEnhancer from "../../components/admin/ProductNewStatusEnhancer";
 import ProductStockEnhancer from "../../components/admin/ProductStockEnhancer";
 
 const AdminProducts = () => (
   <>
     <ProductBrandEnhancer />
     <ProductDescriptionEnhancer />
+    <ProductNewStatusEnhancer />
     <ProductStockEnhancer />
     <AdminProductsLegacy />
   </>
