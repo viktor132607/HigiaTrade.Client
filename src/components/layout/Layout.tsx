f3d799dc7c67faa95a97a34f39d83d780d864526
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-24 20:51:10 +0300

    Enable cart hover preview

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import CartHoverPreview from "./CartHoverPreview";
 
 type LayoutProps = {
   children?: ReactNode;
@@ -11,6 +12,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex min-h-screen flex-col overflow-x-clip">
       <Navbar />
+      <CartHoverPreview />
       <main className="min-w-0 flex-1">
         {children ?? <Outlet />}
       </main>
