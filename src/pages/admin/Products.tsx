f34ca0ca41985039156fb8e7dfe50f84852518b1
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-24 19:43:22 +0300

    Enable invoice-based stock management in product editor

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AdminProductsLegacy from "../../components/admin/AdminProductsLegacy";
 import ProductDescriptionEnhancer from "../../components/admin/ProductDescriptionEnhancer";
+import ProductStockEnhancer from "../../components/admin/ProductStockEnhancer";
 
 const AdminProducts = () => (
   <>
     <ProductDescriptionEnhancer />
+    <ProductStockEnhancer />
     <AdminProductsLegacy />
   </>
 );
