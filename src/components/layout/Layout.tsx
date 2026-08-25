9812b9d59af37d211535bdc6b4971e5420e5e98f
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-25 19:42:16 +0300

    Show category breadcrumb on product pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import CartHoverPreview from "./CartHoverPreview";
 import ProductSearchEnhancer from "./ProductSearchEnhancer";
 import CompareNavbarEnhancer from "./CompareNavbarEnhancer";
+import ProductBreadcrumb from "./ProductBreadcrumb";
 import CompareTray from "../products/CompareTray";
 import SeoManager from "../seo/SeoManager";
 
@@ -21,6 +22,7 @@ const Layout = ({ children }: LayoutProps) => {
       <CompareNavbarEnhancer />
       <CartHoverPreview />
       <CompareTray />
+      <ProductBreadcrumb />
       <main className="min-w-0 flex-1">
         {children ?? <Outlet />}
       </main>
