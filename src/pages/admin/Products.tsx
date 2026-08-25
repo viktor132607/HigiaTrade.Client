a49a868a7d56b966d2be4e74f98e497f77443fa1
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-25 18:11:20 +0300

    Show invoice product completion notice

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AdminProductsLegacy from "../../components/admin/AdminProductsLegacy";
 import ProductBrandEnhancer from "../../components/admin/ProductBrandEnhancer";
 import ProductDescriptionEnhancer from "../../components/admin/ProductDescriptionEnhancer";
+import ProductIncompleteNotice from "../../components/admin/ProductIncompleteNotice";
 import ProductNewStatusEnhancer from "../../components/admin/ProductNewStatusEnhancer";
 import ProductStockEnhancer from "../../components/admin/ProductStockEnhancer";
 
@@ -11,6 +12,7 @@ const AdminProducts = () => (
     <ProductDescriptionEnhancer />
     <ProductNewStatusEnhancer />
     <ProductStockEnhancer />
+    <ProductIncompleteNotice />
     <AdminProductsLegacy />
   </>
 );
