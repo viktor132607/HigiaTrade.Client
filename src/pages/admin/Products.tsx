198113dab0c325a70371893b1b9b5738372d8546
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-26 16:40:19 +0300

    Use category hierarchy in product editor

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AdminProductsLegacy from "../../components/admin/AdminProductsLegacy";
 import ProductAvailabilitySortEnhancer from "../../components/admin/ProductAvailabilitySortEnhancer";
 import ProductBrandEnhancer from "../../components/admin/ProductBrandEnhancer";
+import ProductCategoryHierarchyEnhancer from "../../components/admin/ProductCategoryHierarchyEnhancer";
 import ProductDescriptionEnhancer from "../../components/admin/ProductDescriptionEnhancer";
 import ProductIncompleteNotice from "../../components/admin/ProductIncompleteNotice";
 import ProductNewStatusEnhancer from "../../components/admin/ProductNewStatusEnhancer";
@@ -11,6 +12,7 @@ const AdminProducts = () => (
   <>
     <ProductAvailabilitySortEnhancer />
     <ProductBrandEnhancer />
+    <ProductCategoryHierarchyEnhancer />
     <ProductDescriptionEnhancer />
     <ProductNewStatusEnhancer />
     <ProductStockEnhancer />
