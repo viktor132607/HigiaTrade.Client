72b2e90f0520a11f30ab690f18446f7ba43ec9b9
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-08-27 14:18:32 +0300

    Enable live brand default image preview

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -3,6 +3,7 @@ import AdminProductsLegacy from "../../components/admin/AdminProductsLegacy";
 import ProductAvailabilitySortEnhancer from "../../components/admin/ProductAvailabilitySortEnhancer";
 import ProductBrandEnhancer from "../../components/admin/ProductBrandEnhancer";
 import ProductCategoryHierarchyEnhancer from "../../components/admin/ProductCategoryHierarchyEnhancer";
+import ProductDefaultImagePreviewEnhancer from "../../components/admin/ProductDefaultImagePreviewEnhancer";
 import ProductDescriptionEnhancer from "../../components/admin/ProductDescriptionEnhancer";
 import ProductIncompleteNotice from "../../components/admin/ProductIncompleteNotice";
 import ProductNewStatusEnhancer from "../../components/admin/ProductNewStatusEnhancer";
@@ -12,6 +13,7 @@ const AdminProducts = () => (
   <>
     <ProductAvailabilitySortEnhancer />
     <ProductBrandEnhancer />
+    <ProductDefaultImagePreviewEnhancer />
     <ProductCategoryHierarchyEnhancer />
     <ProductDescriptionEnhancer />
     <ProductNewStatusEnhancer />
